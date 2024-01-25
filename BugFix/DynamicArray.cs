c490e881085f70e2d3187f31261f4bde42224c08
X6STRIVE <X6STRIVE@IN05N001NS>
2024-01-25 15:52:40 +0530

    Add project files.

---
+++ BugFix/DynamicArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BugFix
+{
+    public class DynamicArray<T>
+    {
+        private T[] baseArray;
+        public int Count { get; private set; }
+
+        public DynamicArray(int size)
+        {
+            baseArray = new T[size];
+            Count = 0;
+        }
+
+        public void Add(int index, T item)
+        {
+            
+            if (index >= baseArray.Length)
+            {
+                Resize(index);
+            }
+
+            // Shift elements to make space for the new item
+            for (int i = Count - 1; i >= index; i--)
+            {
+                baseArray[i + 1] = baseArray[i];
+            }
+
+            baseArray[index] = item;
+            Count++;
+        }
+
+        private void Resize(int sz)
+        {
+            int newSize = sz * 2;
+            T[] temp = new T[newSize];
+            Array.Copy(baseArray, temp, Count);
+            baseArray = temp;
+        }
+        // Indexer
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return baseArray[index];
+            }
+        }
+    }
+}
